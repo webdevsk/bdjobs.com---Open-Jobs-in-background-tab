6fa85ab81b41b8882cef2947adde5e882347d20c
Mohammed Salman Khan <72684302+webdevsk@users.noreply.github.com>
2024-09-01 12:26:32 +0600

    Hotfix

--- bdjobs.com---Open-Jobs-in-background-tab.userscript.js
+++ bdjobs.com---Open-Jobs-in-background-tab.userscript.js
@@ -3,10 +3,11 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://jobs.bdjobs.com/*
 // @grant       GM_openInTab
-// @version     1.0
+// @version     1.1
 // @author      webdevsk
 // @run-at      document-end
 // @description Opens Job pages in a background tab
 // ==/UserScript==
-unsafeWindow.DivOpen = QStr => GM_openInTab("jobdetails.asp?" + QStr, { active: false })
-unsafeWindow.DivOpenBN = QStr => GM_openInTab("jobdetailsBN.asp?" + QStr, { active: false })
+unsafeWindow.DivOpen = QStr => {windowOpen == 0 && GM_openInTab("jobdetails.asp?" + QStr, {active: false}); windowOpen = 0}
+unsafeWindow.DivOpenBN = QStr => {windowOpen == 0 && GM_openInTab("jobdetailsBN.asp?" + QStr, {active: false}); windowOpen = 0}
+
